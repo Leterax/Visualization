55db324b0512f3734f2444394a035c63a6bce4ad
Leterax <noneofyourbuisness@gmail.com>
2019-10-31 19:22:29 +0100

    working text!

--- Demo1/main.py
+++ Demo1/main.py
@@ -6,6 +6,8 @@ import moderngl_window as mglw
 import numpy as np
 from pyrr import matrix44
 
+from Demo1.generate_text import render_text_perimeter_balls
+
 
 class BasicWindowConfig(mglw.WindowConfig):
     """Minimal WindowConfig example"""
@@ -18,15 +20,14 @@ class BasicWindowConfig(mglw.WindowConfig):
 
         self.prog = self.load_program("shader.glsl")
         self.prog['time'].value = 0.5
-        projection = matrix44.create_orthogonal_projection(*(0, 0), *self.wnd.buffer_size, -1.0, 1.0, dtype='f4')
+        projection = matrix44.create_orthogonal_projection(0, self.wnd.size[0], 0, self.wnd.size[1], -1.0, 1.0,
+                                                           dtype='f4')
         self.prog['m_proj'].write(projection.tobytes())
 
-        n = 1_000_000_000_00
-        # target = render_text_perimeter_balls("Hey!", scale=300, pos=(75, 250))
-        # n = target.shape[0]
-        vertices = (np.random.random_sample((n, 2)) - .5) * 2
-
-        target = (np.random.random_sample((n, 2)) - .5) * 2
+        target = render_text_perimeter_balls("Hey!", scale=300, pos=(75, 250))
+        n = target.shape[0]
+        vertices = np.random.random_sample((n, 2))
+        vertices = np.array([vertices[:, 0] * self.window_size[0], vertices[:, 1] * self.window_size[1]]).T
 
         self.vbo_1 = self.ctx.buffer(vertices.astype('f4').tobytes())
         self.vbo_2 = self.ctx.buffer(target.astype('f4').tobytes())
