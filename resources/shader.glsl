540971142f99226b3aa5818f62ee06fbb96a30a8
Leterax <noneofyourbuisness@gmail.com>
2019-10-31 19:36:04 +0100

    added __init__.py

--- resources/shader.glsl
+++ resources/shader.glsl
@@ -13,6 +13,33 @@ void main() {
     gl_Position = m_proj * vec4(interp, 0.0, 1.0);
 }
 
+#elif defined GEOMETRY_SHADER
+
+layout (points) in;
+layout (triangle_strip, max_vertices = 4) out;
+
+void main() {
+    const float size = 0.01;
+
+    vec2 pos = gl_in[0].gl_Position.xy;
+    vec2 right = vec2(1.0, 0.0) * size;
+    vec2 up = vec2(0.0, 1.0) * size;
+
+    gl_Position = vec4(pos + (right + up), 0.0, 1.0);
+    EmitVertex();
+
+    gl_Position = vec4(pos + (-right + up), 0.0, 1.0);
+    EmitVertex();
+
+    gl_Position = vec4(pos + (right - up), 0.0, 1.0);
+    EmitVertex();
+
+    gl_Position = vec4(pos + (-right - up), 0.0, 1.0);
+    EmitVertex();
+
+    EndPrimitive();
+}
+
 #elif defined FRAGMENT_SHADER
 
 out vec4 fragColor;
