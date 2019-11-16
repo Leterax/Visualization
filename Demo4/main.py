96b96705ca20c8d22b19f4ac7932c3a9ea200568
Leterax <noneofyourbuisness@gmail.com>
2019-11-16 12:25:34 +0100

    roating and scaling

--- Demo4/main.py
+++ Demo4/main.py
@@ -18,6 +18,7 @@ CUBE_VERTICES = np.array([
     1.0, 1.0, -1.0,
     -1.0, 1.0, -1.0
 ])
+SHOULD_ROTATE_AND_SCALE = True
 
 
 class ModelMatrix:
@@ -81,17 +82,17 @@ class DroneViz(mglw.WindowConfig):
     """Base class with built in 3D camera support"""
     title = "DroneViz"
     resource_dir = (Path(__file__) / '../resources').absolute()
-    aspect_ratio = 1.0
-    window_size = 1024, 1024
-    resizable = False
-    samples = 4
+    aspect_ratio = None
+    window_size = 1280, 720
+    resizable = True
+    samples = 16
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.wnd.mouse_exclusivity = True
         self.camera = KeyboardCamera(self.wnd.keys, aspect_ratio=self.wnd.aspect_ratio)
         self.camera_enabled = True
         self.render_program = self.load_program('my_shader.glsl')
-        # projection = pyrr.matrix44.create_perspective_projection(90, self.aspect_ratio, 1, 100, dtype='f4')
 
         self.render_program['projection'].write(self.camera.projection.tobytes())
         self.model_matrix = ModelMatrix()
@@ -102,10 +103,25 @@ class DroneViz(mglw.WindowConfig):
 
     def render(self, time, frame_time):
         self.ctx.clear(0)
-        self.ctx.enable(moderngl.BLEND)
-        self.render_program['model'].write(self.model_matrix.matrix.astype('f4').tobytes())
+        self.ctx.enable_only(moderngl.DEPTH_TEST | moderngl.CULL_FACE)
+
+        if SHOULD_ROTATE_AND_SCALE:
+            # rotate cube
+            self.model_matrix.rotate(time % np.pi)
+            self.model_matrix.rotate(time % np.pi, 1)
+            self.model_matrix.rotate(time % np.pi, 2)
+
+            # make scale loop between 2 values
+            v = time % np.pi
+            if v > np.pi / 2:
+                v = np.pi - v
+                self.model_matrix.scale(v, v, v)
+            else:
+                self.model_matrix.scale(v, v, v)
 
         # self.render_program['time'].value = min(time / 1, 1.0)
+        self.render_program['model'].write(self.model_matrix.matrix.astype('f4').tobytes())
+        self.render_program['m_camera'].write(self.camera.matrix.astype('f4').tobytes())
         self.cube.render(self.render_program)
 
     def key_event(self, key, action, modifiers):
@@ -121,6 +137,9 @@ class DroneViz(mglw.WindowConfig):
                 self.wnd.cursor = not self.camera_enabled
             if key == keys.SPACE:
                 self.timer.toggle_pause()
+            if key == keys.H:
+                self.model_matrix.translate(1, 0, 0)
+                print(self.model_matrix.matrix)
 
     def mouse_position_event(self, x: int, y: int, dx, dy):
         if self.camera_enabled:
